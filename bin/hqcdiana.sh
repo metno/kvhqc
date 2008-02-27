a778e44b27a25bf90082bcf6c72af507fd53a9b9
Knut Johansen <knut.johansen@met.no>
2008-02-27 07:52:44 +0000

    git-svn-id: https://svn.met.no/kvoss/kvhqc/trunk@134 422d51e2-5941-41ac-8e65-d5b5931239be

--- bin/hqcdiana.sh
+++ bin/hqcdiana.sh
@@ -4,10 +4,9 @@ export LD_LIBRARY_PATH=/metno/local/lib/mesa:${HOME}/omniORB4/lib
 export DIANADIR=/metno/local/diana
 if [ -z $HQCDIR ]; then export HQCDIR=/metno/local/kvhqc; fi
 
-# FIXME: Vi kan ikke være avhengige av ting fra en kildekode-katalog!
-# FIXME: Vi skal bruke KVALOBS-variabelen, ikke KVDIR. Rett i .cc-koden
 if [ -z $KVALOBS ]; then export KVALOBS=/metno/local/src/kvalobs; fi
 if [ -z $KVDIR   ]; then export KVDIR=${KVALOBS}; fi
+export HIST=0
 
 $DIANADIR/bin/diana -s ${HQCDIR}/diana.setup& &> ~/diana.err
 $HQCDIR/bin/hqc &
