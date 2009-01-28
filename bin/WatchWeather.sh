c5ec1bb6cac7bd47fc8abde6f07fd16a95d9f27e
Knut Johansen <knut.johansen@met.no>
2009-01-28 13:42:09 +0000

    git-svn-id: https://svn.met.no/kvoss/kvhqc/trunk@699 422d51e2-5941-41ac-8e65-d5b5931239be

--- bin/WatchWeather.sh
+++ bin/WatchWeather.sh
@@ -5,5 +5,5 @@ export KVALOBS=$HQCDIR
 export KVDIR=$HQC/kvalobs
 export LD_LIBRARY_PATH=/metno/local/lib/mesa::${HOME}/omniORB4/lib:${KVDIR}/lib:${KVDIR}/lib/kvalobs
 export HIST=0
-$HQCDIR/bin/WatchRR4
+$HQCDIR/bin/Weather4
  
--- bin/hqcdiana.sh
+++ bin/hqcdiana.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
-export HQC=/disk1/ubuntu
 export HQCDIR=$HQC/kvhqc
 export KVALOBS=$HQC/kvhqc
 export KVDIR=$HQC/kvalobs
-#export PATH=/usr/lib/qt4/bin:$PATH	
-#export QTDIR=/usr/lib/qt4
-#export QTLIB=/usr/lib
-#export QTINC=/usr/include
-#export BOOST_HOME=$HQC/apps
-#export PUTOOLS=$HQC/apps
-
-#export LD_LIBRARY_PATH=/metno/local/lib/mesa:${HQC}/omniORB4/lib:${KVDIR}/lib:${KVDIR}/lib/kvalobs
-#export LD_LIBRARY_PATH=${HQC}/omniORB4/lib:${KVDIR}/lib:${KVDIR}/lib/kvalobs
-#export LD_LIBRARY_PATH=${KVDIR}/lib:${KVDIR}/src/service-libs/kvcpp/.libs
 export LD_LIBRARY_PATH=${KVDIR}/src/service-libs/kvcpp/.libs:${KVDIR}/src/lib/kvskel/.libs:${KVDIR}/src/lib/decodeutility/.libs:${KVDIR}/src/lib/kvalobs/.libs:${KVDIR}/src/lib/kvdb/.libs:${KVDIR}/src/lib/fileutil/.libs:${KVDIR}/src/lib/dnmithread/.libs:${KVDIR}/src/lib/milog/.libs:${KVDIR}/src/lib/corbahelper/.libs:${HQC}/local/lib
 export DIANADIR=/usr/local
 if [ -z $HQCDIR ]; then export HQCDIR=/metno/local/kvhqc; fi
