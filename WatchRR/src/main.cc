d1cb037b14ad91b440036e7f31304bf0f36d87fb
Knut Johansen <knut.johansen@met.no>
2009-09-25 06:25:38 +0000

    Ny ldap
    git-svn-id: https://svn.met.no/kvoss/kvhqc/trunk@874 422d51e2-5941-41ac-8e65-d5b5931239be

--- WatchRR/src/main.cc
+++ WatchRR/src/main.cc
@@ -69,7 +69,7 @@ int main( int argc, char ** argv )
 
   QString userName;
   reinserter = Authentication::identifyUser( KvApp::kvApp,
-  					     "ldap.oslo.dnmi.no", userName);
+  					     "ldap-oslo.met.no", userName);
   if ( reinserter == 0 ) {
     int res = QMessageBox::warning( 0, "Autentisering",
 				    "Du er ikke registrert som operatør!\n"
