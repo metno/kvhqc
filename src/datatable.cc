c0da10b2d9bb46e5b5a7b29fe1f7a6614bb5b7a0
Knut Johansen <knut.johansen@met.no>
2009-10-13 12:33:54 +0000

    Justering av kolonnebredde
    git-svn-id: https://svn.met.no/kvoss/kvhqc/trunk@909 422d51e2-5941-41ac-8e65-d5b5931239be

--- src/datatable.cc
+++ src/datatable.cc
@@ -59,8 +59,8 @@ int codeParam[] = {  1,  2,  3,  4,  6,  7,  9, 10, 11, 12,
 		    13, 14, 15, 17, 18, 19, 20, 21, 22, 23,
 		     24, 25, 26, 27, 27, 28, 31, 32, 33, 34,
 		     35, 36, 37, 38, 39, 40, 41, 42, 43, 44,
-		     45, 46, 47, 48, 49,151,301,302,303,304,
-		     305,306,307,308,1021,1022,1025,1026};
+		     45, 46, 47, 48, 49,151,273,301,302,303,
+		     304,305,306,307,308,1021,1022,1025,1026};
 
 DataTable::DataTable(QStringList selPar, 
 		     int noSel, 
@@ -74,7 +74,6 @@ DataTable::DataTable(QStringList selPar,
 		     int ncp,
 		     bool isShTy)
   : Q3Table( 1000, 100, parent, "table" ) {
-
   HqcMainWindow * hmw = getHqcMainWindow( parent );
   BusyIndicator busyIndicator();
 
@@ -149,7 +148,8 @@ DataTable::DataTable(QStringList selPar,
   }
 
   for ( int icol = 0; icol < noColPar*noParam; icol++ ) {
-    adjustColumn(icol);  
+    setColumnWidth(icol,50);
+    //    adjustColumn(icol);  
   }
   //
   // Table contents
