43ac9718a4cf3ad6dd84f9577612b2786788f681
Knut Johansen <knut.johansen@met.no>
2009-10-13 12:32:14 +0000

    Endret henting av .png-filer
    git-svn-id: https://svn.met.no/kvoss/kvhqc/trunk@908 422d51e2-5941-41ac-8e65-d5b5931239be

--- src/hqc4.cc
+++ src/hqc4.cc
@@ -79,10 +79,11 @@ int main( int argc, char ** argv ) {
   QString captionSuffix = QString::fromStdString(kvapp.kvpathInCorbaNameserver());
   QString caption = "HQC " + captionSuffix;
   mw->setCaption( caption );
-  //  mw->showMaximized();
+  mw->setIcon( QPixmap("/usr/local/etc/kvhqc/hqc.png") );
   mw->setGeometry(10,10,1268,942);
   a.setMainWidget(mw);
 
+  //  mw->showMaximized();
   mw->show();
   a.connect( &a, SIGNAL(lastWindowClosed()), &a, SLOT(quit()) );
 
