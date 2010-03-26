0bb9042562263bc406dd77627b8f62dd5a4a87fe
Knut Johansen <knut.johansen@met.no>
2010-03-26 13:02:51 +0000

    New file
    git-svn-id: https://svn.met.no/kvoss/kvhqc/trunk@1351 422d51e2-5941-41ac-8e65-d5b5931239be

---
+++ include/rejectdialog.h
@@ -0,0 +1,65 @@
+#ifndef REJECTDIALOG_H
+#define REJECTDIALOG_H
+
+#include <qvariant.h>
+#include <qdialog.h>
+#include <utility>
+#include <qdatetime.h>
+
+class Q3VBoxLayout;
+class Q3HBoxLayout;
+class Q3GridLayout;
+class QSpacerItem;
+class QLabel;
+class QDateTimeEdit;
+class QCheckBox;
+class QPushButton;
+typedef std::pair<QDateTime, QDateTime> TimeSpan;
+
+class RejectDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    RejectDialog( QWidget* parent = 0);
+    //    ~RejectDialog();
+
+    QLabel* textLabel3;
+    QLabel* textLabel1;
+    QLabel* textLabel2;
+    //    QCheckBox* defineTo;
+    QDateTimeEdit* fromEdit;
+    QDateTimeEdit* toEdit;
+    //    QCheckBox* rmXmlCheckBox;
+    QPushButton* okButton;
+    QPushButton* cancelButton;
+
+    TimeSpan getTimeSpan();
+ 
+    QDateTime dtto;
+    QDateTime dtfrom;
+
+public slots:
+    void setFromTime(const QDateTime& dt);
+    void setToTime(const QDateTime& dt);
+    //    Reject getReject();
+    //    bool hideXml();
+    /*
+protected:
+    QVBoxLayout* RejectDialogLayout;
+    QGridLayout* layout2;
+    QHBoxLayout* Layout1;
+    QSpacerItem* Horizontal_Spacing2;
+    
+protected slots:
+    virtual void languageChange();
+
+private slots:
+    void timeRangeCheck();
+    */
+signals:
+    void rejectHide();
+    void rejectApply();
+};
+
+#endif // REJECTDIALOG_H
