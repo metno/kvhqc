12a0fd50b94877b2097ef0bb66c01d463104687b
Alexander Bürger <alexander.buerger@met.no>
2013-09-05 12:34:17 +0000

    flush log4cpp::OstreamAppender after each line
    git-svn-id: https://svn.met.no/kvoss/kvhqc/trunk@4511 422d51e2-5941-41ac-8e65-d5b5931239be

--- authentication/HqcLogging.cc
+++ authentication/HqcLogging.cc
@@ -12,20 +12,56 @@
 
 #include <QtCore/QString>
 
+#include <boost/foreach.hpp>
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+
+namespace /* anonymous */ {
+
+class FlushingOstreamAppender : public log4cpp::OstreamAppender {
+public:
+    FlushingOstreamAppender(const std::string& name, std::ostream* stream)
+        : OstreamAppender(name, stream) { }
+
+protected:
+    virtual void _append(const log4cpp::LoggingEvent& event)
+        { log4cpp::OstreamAppender::_append(event); if (_stream) (*_stream) << std::flush; }
+
+};
+
+log4cpp::Appender* makeConsoleAppender()
+{
+    log4cpp::Appender *a = new FlushingOstreamAppender("console", &std::cout);
+    log4cpp::PatternLayout* layout = new log4cpp::PatternLayout();
+    layout->setConversionPattern("%d %p %c: %m%n");
+    a->setLayout(layout);
+    return a;
+}
+
+} // anonymous namespace
 
 Log4CppConfig::Log4CppConfig(const std::string& l4c_p)
 {
     struct stat sb;
     if (stat(l4c_p.c_str(), &sb) == 0) {
         log4cpp::PropertyConfigurator::configure(l4c_p);
+        std::vector<log4cpp::Category*>* allCat = log4cpp::Category::getCurrentCategories();
+        if (allCat) {
+            BOOST_FOREACH(log4cpp::Category* cat, *allCat) {
+                log4cpp::AppenderSet allApp = cat->getAllAppenders();
+                BOOST_FOREACH(log4cpp::Appender* app, allApp) {
+                    log4cpp::OstreamAppender* oApp = dynamic_cast<log4cpp::OstreamAppender*>(app);
+                    if (!oApp)
+                        continue;
+                    cat->removeAppender(oApp);
+                    cat->addAppender(makeConsoleAppender());
+                }
+            }
+        }
     } else {
-        log4cpp::Appender *a = new log4cpp::OstreamAppender("console", &std::cout);
-        log4cpp::PatternLayout* layout = new log4cpp::PatternLayout();
-        layout->setConversionPattern("%d %p %c: %m%n");
-        a->setLayout(layout);
+        log4cpp::Appender *a = makeConsoleAppender();
 
         log4cpp::Category& root = log4cpp::Category::getRoot();
         root.setPriority(log4cpp::Priority::DEBUG);
