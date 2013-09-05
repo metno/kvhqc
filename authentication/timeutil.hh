40328e91e2cae7b5f0a5e72ef4e36096499d634a
Alexander Bürger <alexander.buerger@met.no>
2013-09-05 12:43:29 +0000

    fixed infinite recursion in timeutil::clearedMinutesAndSeconds
    git-svn-id: https://svn.met.no/kvoss/kvhqc/trunk@4643 422d51e2-5941-41ac-8e65-d5b5931239be

--- authentication/timeutil.hh
+++ authentication/timeutil.hh
@@ -75,7 +75,7 @@ int hourDiff(const ptime& t0, const ptime& t1);
 
 void clearMinutesAndSeconds(QDateTime& dt);
 inline QDateTime clearedMinutesAndSeconds(const QDateTime& dt)
-{ QDateTime dt0(dt); clearedMinutesAndSeconds(dt0); return dt0; }
+{ QDateTime dt0(dt); clearMinutesAndSeconds(dt0); return dt0; }
 
 QDateTime nowWithMinutes0Seconds0();
 
