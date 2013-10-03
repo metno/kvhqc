7a68babf4d97a296b7ba3799bc8f8eb592745051
Vegard Bønes <vegard.bones@met.no>
2013-10-03 09:01:55 +0000

    simplified representation of dates missing
    git-svn-id: https://svn.met.no/kvoss/kvhqc/branches/missinglist@4828 422d51e2-5941-41ac-8e65-d5b5931239be

--- src/hqc/missing/StationOrderedMissingDataModel.cc
+++ src/hqc/missing/StationOrderedMissingDataModel.cc
@@ -3,20 +3,44 @@
 #include <QList>
 #include <boost/foreach.hpp>
 #include <boost/date_time.hpp>
+#include <string>
+#include <sstream>
 
 
 namespace
 {
-QString dateList(const std::set<boost::gregorian::date> & dates)
+std::string getDateRangeString_( const boost::gregorian::date & from, const boost::gregorian::date & to )
 {
-    QString ret;
-    BOOST_FOREACH(const boost::gregorian::date & date, dates) {
-        std::string s_date = to_simple_string(date);
-        if ( not ret.isEmpty() )
-            ret += " ";
-        ret += s_date.c_str();
+  std::string ret = to_simple_string(from);
+  if ( from != to )
+    ret += std::string( " - " ) + to_simple_string(to);
+  return ret;
+}
+
+
+std::string dateList(const std::vector<boost::gregorian::date> & dates)
+{
+    typedef std::vector<boost::gregorian::date>::size_type index;
+
+    if ( dates.empty() )
+      return "";
+
+    std::ostringstream ss;
+
+    boost::gregorian::date current = dates.front();
+    index pos = 0;
+    for ( index i = 1; i < dates.size(); ++ i ) {
+      boost::gregorian::date tmp( current );
+      tmp += boost::gregorian::days(i - pos);
+      if ( dates[i] != tmp ) {
+        ss << getDateRangeString_( current, dates[ i -1 ] ) << ", ";
+        current = dates[ i ];
+        pos = i;
+      }
     }
-    return ret;
+    ss << getDateRangeString_( current, dates[ dates.size() -1 ] );
+
+    return ss.str();
 }
 }
 
@@ -29,23 +53,23 @@ StationOrderedMissingDataModel::StationOrderedMissingDataModel(const MissingList
     headers.push_back("Dates missing");
     setHorizontalHeaderLabels(headers);
 
-    std::map<kvalobs::kvStation, std::set<boost::gregorian::date> > orderedMissingList;
+    std::map<kvalobs::kvStation, std::vector<boost::gregorian::date> > orderedMissingList;
 
     for ( MissingList::const_iterator it = missing.begin(); it != missing.end(); ++ it ) {
         const boost::gregorian::date & date = it->first;
         BOOST_FOREACH ( const kvalobs::kvStation & station, it->second ) {
-            orderedMissingList[station].insert(date);
+            orderedMissingList[station].push_back(date);
         }
     }
 
-    for ( std::map<kvalobs::kvStation, std::set<boost::gregorian::date> >::const_iterator it = orderedMissingList.begin(); it != orderedMissingList.end(); ++ it ) {
+    for ( std::map<kvalobs::kvStation, std::vector<boost::gregorian::date> >::const_iterator it = orderedMissingList.begin(); it != orderedMissingList.end(); ++ it ) {
         const kvalobs::kvStation & station = it->first;
-        const std::set<boost::gregorian::date> & dates = it->second;
+        const std::vector<boost::gregorian::date> & dates = it->second;
 
         QList<QStandardItem *> row;
         row.push_back(new QStandardItem(QString::number(station.stationID())));
         row.push_back(new QStandardItem(QString(station.name().c_str())));
-        row.push_back(new QStandardItem(dateList(dates)));
+        row.push_back(new QStandardItem(QString::fromStdString(dateList(dates))));
 
         appendRow(row);
     }
