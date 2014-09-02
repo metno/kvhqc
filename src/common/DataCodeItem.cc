ffa1ed26647a291a9bd5ad85165813ee53e686c8
Alexander Bürger <alexander.buerger@met.no>
2014-09-02 14:44:23 +0000

    DataCodeItem::data: check that code list exists
    git-svn-id: https://svn.met.no/kvoss/kvhqc/trunk@5168 422d51e2-5941-41ac-8e65-d5b5931239be

--- src/common/DataCodeItem.cc
+++ src/common/DataCodeItem.cc
@@ -12,30 +12,32 @@ DataCodeItem::DataCodeItem(ObsColumn::Type columnType, Code2TextCPtr codes)
 
 QVariant DataCodeItem::data(EditDataPtr obs, const SensorTime& st, int role) const
 {
-  if (role == ObsColumn::ValueTypeRole or role == ObsColumn::TextCodesRole) {
-    const QStringList allCodes = mCodes->allCodes();
-    if (role == ObsColumn::TextCodesRole)
-      return allCodes;
-    int valueTypes = ObsColumn::Numerical;
-    if (not allCodes.empty())
-      valueTypes |= ObsColumn::TextCode;
-    return valueTypes;
-  } else if (role ==  ObsColumn::TextCodeExplanationsRole) {
-    return mCodes->allExplanations();
-  }
-
-  if (not obs)
-    return QVariant();
-
-  if (role == Qt::ForegroundRole) {
-    if (mCodes->isCode(getValue(obs)))
-      return Qt::darkGray;
-  } else if (role == Qt::DisplayRole) {
-    return mCodes->asText(getValue(obs), false);
-  } else if (role == Qt::EditRole) {
-    return "";
-  } else if (role == Qt::TextAlignmentRole) {
-    return Qt::AlignVCenter+(mCodes->isCode(getValue(obs)) ? Qt::AlignLeft : Qt::AlignRight);
+  if (mCodes) {
+    if (role == ObsColumn::ValueTypeRole or role == ObsColumn::TextCodesRole) {
+      const QStringList allCodes = mCodes->allCodes();
+      if (role == ObsColumn::TextCodesRole)
+        return allCodes;
+      int valueTypes = ObsColumn::Numerical;
+      if (not allCodes.empty())
+        valueTypes |= ObsColumn::TextCode;
+      return valueTypes;
+    } else if (role ==  ObsColumn::TextCodeExplanationsRole) {
+      return mCodes->allExplanations();
+    }
+    
+    if (not obs)
+      return QVariant();
+    
+    if (role == Qt::ForegroundRole) {
+      if (mCodes->isCode(getValue(obs)))
+        return Qt::darkGray;
+    } else if (role == Qt::DisplayRole) {
+      return mCodes->asText(getValue(obs), false);
+    } else if (role == Qt::EditRole) {
+      return "";
+    } else if (role == Qt::TextAlignmentRole) {
+      return Qt::AlignVCenter+(mCodes->isCode(getValue(obs)) ? Qt::AlignLeft : Qt::AlignRight);
+    }
   }
   return DataValueItem::data(obs, st, role);
 }
