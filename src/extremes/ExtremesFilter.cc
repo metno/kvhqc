ee8024a8acb9df1140142ef5d1e67f673240dbbe
Alexander Bürger <alexander.buerger@met.no>
2018-06-15 08:00:08 +0200

    bugfix ExtremesFilter from cache

--- src/extremes/ExtremesFilter.cc
+++ src/extremes/ExtremesFilter.cc
@@ -1,3 +1,32 @@
+/*
+  HQC - Free Software for Manual Quality Control of Meteorological Observations
+
+  Copyright (C) 2014-2018 met.no
+
+  Contact information:
+  Norwegian Meteorological Institute
+  Box 43 Blindern
+  0313 OSLO
+  NORWAY
+  email: kvalobs-dev@met.no
+
+  This file is part of HQC
+
+  HQC is free software; you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free
+  Software Foundation; either version 2 of the License, or (at your
+  option) any later version.
+
+  HQC is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+  for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with HQC; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+
 
 #include "ExtremesFilter.hh"
 
@@ -47,32 +76,21 @@ ExtremesFilter::ExtremesFilter(int paramid, int nExtremes)
     const int np=5, p[np] = { 87,88,89,93,95 };
     mParamIds.insert(p, p+np);
   }
-}
 
-QString ExtremesFilter::findExcludedIds() const
-{
-  QString excludedIds;
+  mExcludedIds.clear();
   if (StationInfoBuffer::instance()) {
-    bool first = true;
-    const QString sep = ",";
     const listStat_l& stationDetails = StationInfoBuffer::instance()->getStationDetails();
     for (listStat_l::const_iterator it = stationDetails.begin(); it != stationDetails.end(); ++it) {
       if (Helpers::isNorwegianStationId(it->stationid)
           and (it->municipid <= 100 or it->municipid >= 2100))
       {
-        if (first)
-          first = false;
-        else
-          excludedIds += sep;
-        excludedIds += QString::number(it->stationid);
+        mExcludedIds.insert(it->stationid);
       }
     }
   }
-  return excludedIds;
 }
 
-
-QString ExtremesFilter::acceptingSqlExtraTables(const QString& d, const TimeSpan& time) const
+QString ExtremesFilter::acceptingSqlExtraTables(const QString&, const TimeSpan& time) const
 {
   METLIBS_LOG_SCOPE();
 
@@ -81,15 +99,13 @@ QString ExtremesFilter::acceptingSqlExtraTables(const QString& d, const TimeSpan
       + "FROM data AS dd "
       "   WHERE " + QString::fromStdString(Helpers::isNorwegianStationIdSQL("dd.stationid"));
 
-  const QString excludedIds = findExcludedIds();
-  if (not excludedIds.isEmpty())
-    sql += " AND dd.stationid NOT IN (" + excludedIds + ")";
-
-  sql += "   AND " + set2sql("dd.paramid", mParamIds) +
-      "   AND " + exists_in_obspgm("dd.") +
-      "   AND (substr(dd.useinfo,3,1) IN ('0','1','2')"
-      "        OR (substr(dd.useinfo,3,1) = '3' AND dd.original = dd.corrected))"
-      "   AND dd.obstime " + timespan2sql(time);
+  if (!mExcludedIds.empty())
+    sql += " AND NOT (" + set2sql("dd.stationid", mExcludedIds) + ")";
+  sql += " AND " + set2sql("dd.paramid", mParamIds);
+  sql += " AND " + exists_in_obspgm("dd.");
+  sql += " AND (substr(dd.useinfo,3,1) IN ('0','1','2')"
+         "        OR (substr(dd.useinfo,3,1) = '3' AND dd.original = dd.corrected))";
+  sql += " AND dd.obstime " + timespan2sql(time);
 
   // exclude some special values (no measurement, no snow, no rain, ...)
   if (mParamId == kvalobs::PARAMID_RR_24)
@@ -106,11 +122,11 @@ QString ExtremesFilter::acceptingSqlExtraTables(const QString& d, const TimeSpan
 
 QString ExtremesFilter::acceptingSql(const QString& d, const TimeSpan&) const
 {
-  QString sql = d + "stationid = ex.s"
-      " AND " + d + "corrected = ex.c"
-      " AND " + set2sql(d + "paramid", mParamIds) +
-      " AND " + exists_in_obspgm(d) +
-      " ORDER BY " + d + "corrected " + (mFindMaximum ? "DESC" : "ASC") + ", " + d + "stationid, " + d + "obstime";
+  QString sql = d + "stationid = ex.s";
+  sql += " AND " + d + "corrected = ex.c";
+  sql += " AND " + set2sql(d + "paramid", mParamIds);
+  sql += " AND " + exists_in_obspgm(d);
+  sql += " ORDER BY " + d + "corrected " + (mFindMaximum ? "DESC" : "ASC") + ", " + d + "stationid, " + d + "obstime";
   return sql;
 }
 
@@ -127,7 +143,39 @@ bool ExtremesFilter::subsetOf(ObsFilter_p other) const
 
 bool ExtremesFilter::accept(ObsData_p obs, bool afterSQL) const
 {
-  if (afterSQL or mParamIds.count(obs->sensorTime().sensor.paramId))
+  if (afterSQL)
     return true;
-  return false;
+
+  const int obsStationId = obs->sensorTime().sensor.stationId;
+  if (!Helpers::isNorwegianStationId(obsStationId) || mExcludedIds.count(obsStationId))
+    return false;
+
+  const int obsParamId = obs->sensorTime().sensor.paramId;
+  if (!mParamIds.count(obsParamId))
+    return false;
+
+  // FIXME: check obs_pgm
+
+#if 0
+  const int ui2 = obs->useinfo().flag(2);
+#else
+  kvalobs::kvUseInfo obsUI;
+  obsUI.setUseFlags(obs->controlinfo());
+  const int ui2 = obsUI.flag(2);
+#endif
+  if (!(ui2 == 0 || ui2 == 1 || ui2 == 2 || (ui2 == 3 && obs->original() == obs->corrected())))
+    return false;
+
+  // not needed: check obstime
+
+  // exclude some special values (no measurement, no snow, no rain, ...)
+  if (obsParamId == kvalobs::PARAMID_RR_24) {
+    if (obs->corrected() == -1)
+      return false;
+  } else if (obsParamId == kvalobs::PARAMID_SA) {
+    if (obs->corrected() == -3 || obs->corrected() == -1 || obs->corrected() == 0)
+      return false;
+  }
+
+  return true;
 }
